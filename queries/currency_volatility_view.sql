f063ca2d6e930acf06efa8a8838570a3773a318d
druizf85 <druizf85@gmail.com>
2026-01-12 09:35:22 -0500

    new files loaded

--- queries/currency_volatility_view.sql
+++ queries/currency_volatility_view.sql
@@ -1,5 +1,3 @@
--- Interpretación: monedas más inestables frente al USD.
-
 CREATE OR REPLACE VIEW currency_volatility_view AS
 
 WITH oct_25_vol AS (
@@ -26,24 +24,17 @@ nov_25_vol AS (
 	GROUP BY exchange_rate
 	),
 
--- Here will be the upcomming months and will be updated when needed
-
-
---dec_25_vol AS(
---	SELECT exchange_rate,
---		    COALESCE(STDDEV(values), 0) AS volatility,
---		    AVG(values)    AS avg_value,
---		    MAX(values)    AS max_value,
---		    MIN(values)    AS min_value
---	FROM "Exchange_Rates_Silver_Table"
---		WHERE EXTRACT(MONTH FROM CAST(time_last_update_utc AS date)) = 12
---		AND EXTRACT(YEAR FROM CAST(time_last_update_utc AS date)) = 2025
---	GROUP BY exchange_rate
---	),
-
-
-
--- At last the overall year performance for each exchange_rate
+dec_25_vol AS (
+	SELECT exchange_rate,
+		    COALESCE(STDDEV(values), 0) AS volatility,
+		    AVG(values)    AS avg_value,
+		    MAX(values)    AS max_value,
+		    MIN(values)    AS min_value
+	FROM "Exchange_Rates_Silver_Table"
+		WHERE EXTRACT(MONTH FROM CAST(time_last_update_utc AS date)) = 12
+		AND EXTRACT(YEAR FROM CAST(time_last_update_utc AS date)) = 2025
+	GROUP BY exchange_rate
+	),
 
 all_2025_vol AS(
 	SELECT exchange_rate,
@@ -67,6 +58,10 @@ SELECT m.continent,
 		n_25.max_value AS max_value_november,
 		n_25.avg_value AS avg_value_november,
 		n_25.volatility AS volatility_value_november,
+		d_25.min_value AS min_value_december, 
+		d_25.max_value AS max_value_december,
+		d_25.avg_value AS avg_value_december,
+		d_25.volatility AS volatility_value_december,
 		y_25.min_value AS min_value_2025_all, 
 		y_25.max_value AS max_value_2025_all,
 		y_25.avg_value AS avg_value_2025_all,
@@ -75,6 +70,7 @@ SELECT m.continent,
 FROM all_2025_vol y_25
 JOIN oct_25_vol o_25 ON o_25.exchange_rate = y_25.exchange_rate
 JOIN nov_25_vol n_25 ON n_25.exchange_rate = y_25.exchange_rate
+JOIN dec_25_vol d_25 ON d_25.exchange_rate = y_25.exchange_rate
 JOIN currency_metadata m ON y_25.exchange_rate = m.currency_code
 
 ORDER BY volatility_2025_all DESC
